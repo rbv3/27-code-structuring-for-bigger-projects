7fc319dc2313d80a9d0178a2595172e56dbd5e35
Rodrigo Valenca <rvalenca@amazon.com>
2023-10-08 00:10:53 -0300

    debugging environment

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -6,38 +6,84 @@ export default class Environment {
         this.experience = new Experience()
         this.scene = this.experience.scene
         this.resources = this.experience.resources
+        this.debug = this.experience.debug
+
+        if(this.debug.active) {
+            this.debugFolder = this.debug.ui.addFolder('environment')
+        }
 
         this.setSunLight()
         this.setEnvironmentMap()
     }
 
     setSunLight() {
-        this.sunLight = new THREE.DirectionalLight('#ffffff', 4)
+        this.sunLight = new THREE.DirectionalLight('#ffffff', 2)
         this.sunLight.castShadow = true
         this.sunLight.shadow.camera.far = 15
         this.sunLight.shadow.mapSize.set(1024, 1024)
         this.sunLight.shadow.normalBias = 0.05
         this.sunLight.position.set(3.5, 2, - 1.25)
         this.scene.add(this.sunLight)
+
+        // Debug
+        if(this.debug.active) {
+            this.debugFolder
+                .add(this.sunLight, 'intensity')
+                .name('sunLightIntensity')
+                .min(0)
+                .max(10)
+                .step(0.01)
+            this.debugFolder
+                .add(this.sunLight.position, 'x')
+                .name('sunLightX')
+                .min(-5)
+                .max(5)
+                .step(0.01)
+            
+            this.debugFolder
+                .add(this.sunLight.position, 'y')
+                .name('sunLightY')
+                .min(0)
+                .max(5)
+                .step(0.01)
+            
+            this.debugFolder
+                .add(this.sunLight.position, 'z')
+                .name('sunLightZ')
+                .min(-5)
+                .max(5)
+                .step(0.01)
+        }
     }
     setEnvironmentMap() { 
         this.environmentMap = {}
-        this.environmentMap.intesity = 0.4
+        this.environmentMap.intensity = 0.4
         this.environmentMap.colorSpace = THREE.SRGBColorSpace
         this.environmentMap.texture = this.resources.items.environmentMapTexture
 
         this.scene.environment = this.environmentMap.texture
 
-        this.setEnvironmentMap.updateMaterial = () => {
+        this.environmentMap.updateMaterials = () => {
             this.scene.traverse((child) => {
                 if(child instanceof THREE.Mesh &&
                     child.material instanceof THREE.MeshStandardMaterial) {
                         child.material.envMap = this.environmentMap.texture
-                        child.material.envMapIntensity = this.environmentMap.intesity
+                        child.material.envMapIntensity = this.environmentMap.intensity
                         child.material.needsUpdate = true                       
                     }
             })
         }
-        this.setEnvironmentMap.updateMaterial()
+        this.environmentMap.updateMaterials()
+
+        // Debug
+        if(this.debug.active) {
+            this.debugFolder
+                .add(this.environmentMap, 'intensity')
+                .name('envMapIntensity')
+                .min(0)
+                .max(5)
+                .step(0.1)
+                .onChange(this.environmentMap.updateMaterials)
+        }
     }
 }
